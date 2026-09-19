6c1c6b033e38045271a928b8089a31f9f10c4c0a
polantamba <antambapaul2005@gmail.com>
2026-09-18 20:32:35 -0500

    Cambios finales pol

--- tarjetas/views.py
+++ tarjetas/views.py
@@ -4,7 +4,7 @@ from .services import obtener_cartas_blue_eyes, obtener_carta_por_id
 
 def lista_tarjetas(request):
     cartas = obtener_cartas_blue_eyes()
-    paginator = Paginator(cartas, 12)
+    paginator = Paginator(cartas, 8)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'tarjetas/lista_tarjetas.html', {'page_obj': page_obj})
